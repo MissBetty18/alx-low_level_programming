b3889babd155d4546f14d0a8f53e981e45493f48
MissBetty18 <ruterebeatrice@gmail.com>
2023-04-14 02:52:42 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
-/*
+int evaluate_prime(int n, int i);
+
+/**
  * is_prime_number - finds prime numbers
  *
  * @n: number to check for prime numbers
@@ -12,23 +14,22 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
+	return (evaluate_prime(n, n - 1));
 }
 
 /**
- * check_prime - finds prime numbers
+ * evaluate_prime - finds prime numbers
  *
  * @n: number to calculate its prime numbers
  * @i: iterator
  *
  * Return: 1 if a prime number and 0 if not
  */
-int check_prime(int n, int i)
+int evaluate_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
-	if (n % i == 0 && i > 1)
+	if (n % i == 0 && i > 0 )
 		return (0);
-	if ((n / i) < i)
-		return (1);
-	return (check_prime(n, i + 1));
+	return (evaluate_prime(n, i - 1));
 }
