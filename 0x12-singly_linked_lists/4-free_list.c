2b9cf18abbb2982f9aa8a7daa9bf055ef9edb5a4
MissBetty18 <ruterebeatrice@gmail.com>
2023-04-28 13:31:42 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,10 +8,13 @@ void free_list(list_t *head)
 {
 	list_t *temp;
 
-	while (head != NULL);
+	while (head)
 	{
-		temp = head;
-		head = head->next;
-		free(temp);
+		if (head != NULL)
+		{
+			temp = head;
+			head = head->next;
+			free(temp);
+		}
 	}
 }
